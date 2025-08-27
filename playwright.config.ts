0a05b18d2957c1440e2229de91875e411dfaf104
DEV-RDS01\marben.dimson <marben.dimson@hostednetwork.com.au>
2025-08-27 16:14:53 +1000

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: false,
+    headless: true,
     viewport: null,
     launchOptions: {
       args: ['--start-maximized'],
