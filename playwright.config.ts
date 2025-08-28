4b18e25488e83e98d1def6a7d9be9011938173d9
DEV-RDS01\marben.dimson <marben.dimson@hostednetwork.com.au>
2025-08-28 17:20:55 +1000

    no message

--- playwright.config.ts
+++ playwright.config.ts
@@ -179,7 +179,7 @@ export default defineConfig({
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
             '--disable-gpu',
-            '--start-maximized', // ✅ safe here only
+            //'--start-maximized', // ✅ safe here only
           ],
         },
       },
